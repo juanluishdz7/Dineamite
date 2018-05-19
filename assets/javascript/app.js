c3c943ce7858075c29af0d6921a2d11e5309bc98
danielt812 <danielt812@gmail.com>
2018-05-19 12:12:12 -0500

    gettin values

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,21 +7,22 @@ var healthLabels = [];
 //================================================================
 //EDEMAM AJAX CALL
 //----------------------------------------------------------------
-function displayFoodResults()
-{
-    var queryURL = "https://api.edamam.com/search?q=" + foodSearch + "&app_id=b06e5d63&app_key=0545c2932c4566a17980ea67576b8879&healthLabels=" + healthLabels;
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-    console.log(response);
-    });
-}
+// function displayFoodResults()
+// {
+//     var queryURL = "https://api.edamam.com/search?q=" + foodSearch + "&app_id=b06e5d63&app_key=0545c2932c4566a17980ea67576b8879&healthLabels=" + healthLabels;
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//     }).then(function(response){
+//     console.log(response);
+//     });
+// }
 
 //EVENT HANDLERS
 //================================================================
 $("#submit").on("click", function(){
     event.preventDefault();
-    
+    var checkedBox = $(".checkbox").val();
+    console.log(checkedBox)
 })
 })

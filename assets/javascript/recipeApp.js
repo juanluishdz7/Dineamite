a5135dc2435802a81e8e5ca6696edf6264d468d6
danielt812 <danielt812@gmail.com>
2018-05-23 00:31:01 -0500

    changed h3's on recipe.html

--- assets/javascript/recipeApp.js
+++ assets/javascript/recipeApp.js
@@ -1,12 +1,20 @@
-$(document).ready(function(){
-
+$(document).ready(function()
+{
+    //GLOBAL VARIABLES
+    //===================================================
     var imageResponse = localStorage.getItem("response");
     var stringResponse = JSON.parse(imageResponse);
     console.log(stringResponse);
-    for (i = 0; i < 8; i++) {
+    //Run for loop across 8 hits in ajax call
+    for (i = 0; i < 8; i++)
+    {
+        //Change header on page to ajax response
+        var recipeName = stringResponse.hits[i].recipe.label;
+        $("#header" + [i]).text(recipeName)
+        //Change image on page to ajax response
         var recipeImage = stringResponse.hits[i].recipe.image;
         $("#image" + [i]).attr("src", recipeImage);
         console.log (recipeImage);
-        };
+    };
 });
 
